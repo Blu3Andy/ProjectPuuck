d400362e54a5ec7f8005f2562aa8290f30d9e5c8
Andreas Hahn <BlueAndyDev@gmail.com>
2025-10-30 00:42:23 +0100

    spawn feature

---
+++ Assets/Scripts/Lobby/SpawnPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    // One Way Script
+    [SerializeField] private Transform spawn;
+    [SerializeField] private float knochBackRadius = 5f;
+    [SerializeField] private float knochBackForce = 50f; 
+
+    public void Execute(GameObject player)
+    {
+        player.transform.position = spawn.position;
+
+        player.GetComponent<Rigidbody>().AddExplosionForce(knochBackForce, transform.position + new Vector3(0, 0, -2), knochBackRadius);
+    }
+
+    public void makeBoom()
+    {
+        var surroundingObjects = Physics.OverlapSphere(spawn.position , knochBackRadius);
+        print(surroundingObjects);
+
+        foreach(var obj in surroundingObjects)
+        {
+
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+            if(rb == null) continue;
+
+            
+        }
+    }
+}
