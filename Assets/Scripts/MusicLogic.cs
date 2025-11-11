783483087abb5564d3e5d9a10b9ff6dd475dd3ec
Simon Markiewicz <simon.markiewicz@gmx.de>
2025-11-11 15:24:21 +0100

    MusicLogic changed

--- Assets/Scripts/MusicLogic.cs
+++ Assets/Scripts/MusicLogic.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class MusicLogic : MonoBehaviour
 {
+    public float durationOfEffect;
     private AudioLowPassFilter lowPassTrackFilter;
+    private bool startTimer = false;
+    public float initTime;
 
     void Start()
     {
         lowPassTrackFilter = gameObject.GetComponent<AudioLowPassFilter>();
+        initTime = durationOfEffect;
     }
 
     public void ChangeMusicFilter()
     {
-        lowPassTrackFilter.cutoffFrequency = 22000;   
+        startTimer = true;
+        lowPassTrackFilter.cutoffFrequency = 22000;
     }
+
+    void Update()
+    {
+        if (startTimer)
+        {
+            if (initTime >= 0.01f)
+            {
+                initTime -= Time.deltaTime;
+            }
+            else
+            {
+                lowPassTrackFilter.cutoffFrequency = 400;
+                initTime = durationOfEffect;
+                startTimer = false;
+            }
+        }
+    }
+    
 
 
 }
